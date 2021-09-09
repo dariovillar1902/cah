841d8d32ced8df03eaa018810713606cbe351546
Dario Villar <dario_villar2001@hotmail.com>
2021-09-09 18:41:31 -0300

    prueba26

--- src/scenes/salaDeEspera.js
+++ src/scenes/salaDeEspera.js
@@ -33,7 +33,7 @@ export default class salaDeEspera extends Phaser.Scene {
     create() {
         let self = this;
 
-          this.socket = io('hdpjuego.herokuapp.com:36655', {
+          this.socket = io('hdpjuego.herokuapp.com', {
             path: '/socket.io',
             transports: ['websocket']
         })
